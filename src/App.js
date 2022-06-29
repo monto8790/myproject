60da038eebced8a2e504344cd47075aa6e4cffe6
yungi park <yungipark@intellicode.co.kr>
2022-06-29 17:06:38 +0900

    gh push

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ImageSlide from "./components/ImageSlide";
 function App() {
   return (
     <div>
+      {/* <img src={process.env.PUBLIC_URL + `/public_images/img2.jpg`}></img> */}
       <ImageSlide />
     </div>
   );
--- src/components/ImageSlide.js
+++ src/components/ImageSlide.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 import ImageGallery from "react-image-gallery";
+// import one from "../images/img1.jpg";
 import one from "../images/img1.jpg";
+let default_url = `${process.env.PUBLIC_URL}`;
 const images = [
   {
-    original: require("../images/img2.jpg"),
-    thumbnail: require("../images/img2.jpg"),
-    fullscreen: require("../images/img2.jpg"),
+    original: default_url + "/public_images/img2.jpg",
+    thumbnail: default_url + "/public_images/img2.jpg",
+    fullscreen: default_url + "/public_images/img2.jpg",
   },
   {
-    original: require("../images/img3.jpg"),
-    thumbnail: require("../images/img3.jpg"),
-    fullscreen: require("../images/img3.jpg"),
+    original: default_url + "/public_images/img3.jpg",
+    thumbnail: default_url + "/public_images/img3.jpg",
+    fullscreen: default_url + "/public_images/img3.jpg",
   },
   {
     original: one,
